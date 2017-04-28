b74c0b7a9ed8f540fc3772942a9c2f181f6ce1b9
Haphantk <haphantk37@gmail.com>
2017-04-28 08:10:09 +0700

    them vao 3

---
+++ vd2.sh
@@ -0,0 +1,28 @@
+echo -e "1. Caffe den\n "
+echo -e "2. Caffe sua\n"
+echo -e "3. Nuoc cam\n"
+echo -e "4. sting\n"
+echo " please choose a number: "
+read a
+ if [ $a -eq 1 ]
+then 
+	echo "ban vua chon Caffe den"
+   
+  	if [ $a -eq 2 ]
+	then 
+	 echo "ban vua chon Caffe sua"
+
+ 		if [ $sa -eq 3 ] 
+		then
+		echo"ban vua chon Nuoc cam"
+
+			if [ $a -eq 4 ]
+			then 
+			echo "ban vua chon Sting"
+
+else
+	echo"khong co so nay trong danh sach"
+fi
+fi
+fi
+fi
